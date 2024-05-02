02cd64ab5d4b7528339dda1eed1b2dce8910dc2b
student <student@WIN-6FC6GNDQEIP>
2024-05-02 17:58:35 +0900

    5.1.

--- src/src/src/exam09/Exam2.java
+++ src/src/src/exam09/Exam2.java
@@ -3,22 +3,23 @@ package exam09;
 public class Exam2 {
 
 	public static void main(String[] args) {
-		int[] ary;
-		ary = new int[5];
-		ary[0] = 1;
-		ary[1] = 5;
-		ary[2] = 4;
-		ary[3] = 3;
-		ary[4] = 2;
-
+		//int[] ary;
+		//ary = new int[5];
+		//ary[0] = 1;
+		//ary[1] = 5;
+		//ary[2] = 4;
+		//ary[3] = 3;
+		//ary[4] = 2;
+		int[] ary = {1, 5, 4, 3, 2};
+		
+		// 拡張for文とif文を利用して奇数の値だけを出力する 
 		for(int data : ary) {
+		     if(data % 2 !=0) {
 			System.out.println(data);
-		}
-	
-		if((ary) % 2 ==1) {
-			System.out.println(ary);
-			
-		}
+			continue;
+		     }
+		System.out.println(data);
 	}
 
 }
+	}
